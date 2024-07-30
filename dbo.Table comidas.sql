f35b353d4d65a0e8499889a353fddd28b06624c0
Jorgep1995 <jbeto1397@gmail.com>
2024-07-30 16:45:23 -0600

    Add files via upload

---
+++ dbo.Table comidas.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Comidas]
+(
+	[NombreEmpleado] VARCHAR(50) NOT NULL PRIMARY KEY, 
+    [NúmeroEmpleado] INT NOT NULL, 
+    [NúmeroCliente] INT NOT NULL, 
+    [Menu] VARCHAR(50) NOT NULL, 
+    [Ubicación] VARCHAR(100) NOT NULL, 
+    [Calificación] INT NOT NULL
+)
